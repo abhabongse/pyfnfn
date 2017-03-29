49fe62d60fda35ba8509667bbacff61eea5f7710
Abhabongse Janthong <abhabongse@gmail.com>
2017-03-28 17:23:12 -0700

    Add package __init__.py back.

---
+++ pyfnfn/__init__.py
@@ -0,0 +1,5 @@
+# Author: Abhabongse Janthong <underneaththeunderneath@gmail.com>
+# More info at https://github.com/abhabongse/pyfnfn
+__all__ = ('FunctionFilenameWrapper', 'fnfnwrap')
+
+from .wrapper import FunctionFilenameWrapper, fnfnwrap
